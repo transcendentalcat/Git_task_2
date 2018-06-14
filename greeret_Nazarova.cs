5e1dae700d5fa23cc3bbc1b52b95d46239b1514c
Your Name <Nina_Nazarova@epam.com>
2018-06-14 15:05:28 +0300

    Changes in method Print

--- greeret_Nazarova.cs
+++ greeret_Nazarova.cs
@@ -1,11 +1,4 @@
- class test
-   {
-       public void print()
-       {
-           Console.WriteLine("Hi everyone!");
-		   Console.WriteLine("Conflict changes");
-       }
-   }
+
    class Program
    {
        static void Main(string[] args)
